b8283d8bd2dec8d2458403fc024a0c19212002b4
emwood <emily.wood@wbd.com>
2024-05-17 11:15:11 +0100

    Seem to have inadvertantly lost some changes. Updated the repo back to a semi-working version

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <conio.h> 
+#include <conio.h>
 #include <windows.h>
-#include <cstdlib> 
-#include <deque>
+#include <cstdlib>
 
-//I was getting build errors before I added this in
 void initGameVariables();
 void renderTheGame();
+void hideCursor();
 int _getch();
 
 #define KEY_UP 72
@@ -14,20 +13,20 @@ int _getch();
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
 
-// Declare the size of the game space
 int gameHeight = 20;
 int gameWidth = 80;
 int score = 0;
 
-int xPos, yPos; // position of the snake head
+int xPos, yPos;
 std::string fruit = "*";
-int fruitX, fruitY; // holds the x and y position of the fruit the snake is trying to eat
-
+int fruitX, fruitY;
 enum snakeDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-snakeDirection snakeDir; //holds the value of the above
-std::deque<std::pair<int, int>> snakeBody; //deque means double ended queue allows me to add body to either end of the snake dependent on direction
+snakeDirection snakeDir;
 bool isGameOver;
 
+const int maxSnakeLength = 100; // Maximum length of the snake
+std::pair<int, int> snakeBody[maxSnakeLength]; // Array to store snake's body
+int snakeLength = 1; // Initial length of the snake
 
 void initGameVariables() {
     isGameOver = false;
@@ -35,17 +34,17 @@ void initGameVariables() {
     fruitY = rand() % gameHeight;
     xPos = gameWidth / 2;
     yPos = gameHeight / 2;
-    score = 0; 
+    score = 0;
     snakeDir = STOP;
-    snakeBody.clear();
-    snakeBody.push_front({ yPos, xPos });
-
+    snakeLength = 1;
+    snakeBody[0] = { yPos, xPos }; // Initialize snake with one segment
 }
+
 void hideCursor() {
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursorInfo;
-    cursorInfo.dwSize = 100; 
-    cursorInfo.bVisible = FALSE; 
+    cursorInfo.dwSize = 100;
+    cursorInfo.bVisible = FALSE;
     SetConsoleCursorInfo(consoleHandle, &cursorInfo);
 }
 
@@ -55,7 +54,7 @@ void renderTheGame() {
 
     // Now we get to drawing the borders of the game
     for (int i = 0; i < gameWidth; i++) {
-        std::cout <<  "-";
+        std::cout << "-";
     }
     std::cout << std::endl;
 
@@ -67,11 +66,16 @@ void renderTheGame() {
             // Position fruit within the game bounds
             else if (j == fruitX && i == fruitY) {
                 std::cout << fruit;
-                // Position snake head at centre of the screen
-            } else if (j == xPos && i == yPos) {
-                    std::cout << "O";
-            } else {
-                std::cout << " ";
+            }
+            else {
+                bool isSnakeBody = false;
+                for (int k = 0; k < snakeLength; k++) {
+                    if (j == snakeBody[k].second && i == snakeBody[k].first) {
+                        isSnakeBody = true;
+                        break;
+                    }
+                }
+                std::cout << (isSnakeBody ? "O" : " ");
             }
         }
         std::cout << std::endl;
@@ -80,62 +84,74 @@ void renderTheGame() {
     for (int i = 0; i < gameWidth; i++) {
         std::cout << "-";
     }
-
     std::cout << "\nScore: " << score;
 }
 
 void userInput() {
+    if (_kbhit()) {
         switch (_getch()) {
         case KEY_UP:
-            std::cout << "Up";
             snakeDir = UP;
-            yPos--;
             break;
         case KEY_DOWN:
-            std::cout << "Down";
             snakeDir = DOWN;
-            yPos++;
             break;
         case KEY_LEFT:
-            std::cout << "left";
             snakeDir = LEFT;
-            xPos--;
             break;
         case KEY_RIGHT:
-            std::cout << "Right";
             snakeDir = RIGHT;
-            xPos++;
             break;
         }
-
+    }
 }
 
-// Function to update the game state
 void updateGameState() {
+    // Update snake's position based on direction
+    switch (snakeDir) {
+    case UP:
+        yPos--;
+        break;
+    case DOWN:
+        yPos++;
+        break;
+    case LEFT:
+        xPos--;
+        break;
+    case RIGHT:
+        xPos++;
+        break;
+    }
 
     // Check for collision with fruit
     if (xPos == fruitX && yPos == fruitY) {
         score++;
+        // Generate new position for the fruit
         fruitX = rand() % gameWidth;
         fruitY = rand() % gameHeight;
-        snakeBody.push_front({ yPos, xPos });
+        // Increase snake's length
+        snakeLength++;
     }
+
+    // Move the snake's body
+    for (int i = snakeLength - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    snakeBody[0] = { yPos, xPos };
 }
 
-
-int main()
-{
+int main() {
+    hideCursor();
     initGameVariables();
-    renderTheGame();
 
     while (!isGameOver) {
         userInput();
         updateGameState();
         renderTheGame();
-      
+        Sleep(100); // Adjust the frame rate
     }
 
-    std::cout << "Game over! Your final Score: " << score;
+    std::cout << "Game over! Your Score: " << score;
 
     return 0;
 }
